0bd2a55c1c3d396c6dc8e85195ad72d078c0e4ce
admin <saint-wp@163.com>
2023-10-22 16:15:56 +0800

    update

--- sync.sh
+++ sync.sh
@@ -1,21 +1,34 @@
 project_name=$1
+echo "update ssh"
 mkdir /home/runner/.ssh
 curl -o /home/runner/.ssh/id_rsa https://gitee.com/saintvamp/publick_rsa/raw/master/npw
 chmod 600 /home/runner/.ssh/id_rsa
+echo "update ssh done"
+echo "clone github"
 cd /home/runner/
 mkdir temp_d
 cd /home/runner/temp_d
 mkdir github
 cd github
 git clone https://github.com/SaintVamp/$project_name.git
+echo "clone github down"
+echo "clone gitee"
 cd /home/runner/temp_d
 mkdir gitee
 cd gitee
 git clone https://gitee.com/saintvamp/$project_name.git
+echo "clone gitee down"
+pwd
+echo "rm gitee git"
 rm -rf $project_name/.git
+echo "rm gitee git down"
+echo "cp gitee to github"
 cp -r /home/runner/temp_d/gitee/$project_name/ /home/runner/temp_d/github/
+echo "cp gitee to github down"
 cd /home/runner/temp_d/github/$project_name
+echo "git add"
 ls |xargs -i git add -v {}
+echo "git add down"
 git config --global user.email "saint-wp@163.com"
 git config --global user.name "sv-action"
 git config --global core.editor "vi"
