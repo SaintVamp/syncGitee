7025e63333ce60fc05f1377e1c2756d93f4b20b9
SaintVamp <saint-wp@163.com>
2023-10-22 15:59:37 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,3 +1,4 @@
+project_name=$1
 mkdir /home/runner/.ssh
 curl -o /home/runner/.ssh/id_rsa https://gitee.com/saintvamp/publick_rsa/raw/master/npw
 chmod 600 /home/runner/.ssh/id_rsa
@@ -6,14 +7,14 @@ mkdir temp_d
 cd /home/runner/temp_d
 mkdir github
 cd github
-git clone https://github.com/SaintVamp/test_sync.git
+git clone https://github.com/SaintVamp/$project_name.git
 cd /home/runner/temp_d
 mkdir gitee
 cd gitee
-git clone https://gitee.com/saintvamp/test_sync.git
-rm -rf test_sync/.git
-cp -r /home/runner/temp_d/gitee/test_sync/ /home/runner/temp_d/github/
-cd /home/runner/temp_d/github/test_sync
+git clone https://gitee.com/saintvamp/$project_name.git
+rm -rf $project_name/.git
+cp -r /home/runner/temp_d/gitee/$project_name/ /home/runner/temp_d/github/
+cd /home/runner/temp_d/github/$project_name
 ls |xargs -i git add -v {}
 git config --global user.email "saint-wp@163.com"
 git config --global user.name "sv-action"
@@ -21,7 +22,7 @@ git config --global core.editor "vi"
 need_commit=$(git status | grep -c "nothing to commit")
 if [ $need_commit -eq 0 ] ;then
   git commit -m "merge gitee"
-  echo yes |git push git@github.com:SaintVamp/test_sync.git
+  echo yes |git push git@github.com:SaintVamp/$project_name.git
 else
   echo "nothing to commit"
 fi
