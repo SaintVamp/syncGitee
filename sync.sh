6b4c79c3c29f72f483d2cfd19f2909c1bfa53c93
SaintVamp <saint-wp@163.com>
2023-10-22 15:52:18 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -18,9 +18,9 @@ ls |xargs -i git add -v {}
 git config --global user.email "saint-wp@163.com"
 git config --global user.name "sv-action"
 git config --global core.editor "vi"
-git commit -m "merge gitee"
 need_commit=$(git status | grep -c "nothing to commit")
 if [ $need_commit -eq 0 ] ;then
+  git commit -m "merge gitee"
   echo yes |git push git@github.com:SaintVamp/test_sync.git
 else
   echo "nothing to commit"
